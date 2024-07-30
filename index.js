41772073d260a5066558ee31804a3914cd1c4d83
muhammaddiyor <muhammaddiyor20032808@gmail.com>
2024-07-30 18:42:28 +0500

    initial bot codes

---
+++ index.js
@@ -0,0 +1,31 @@
+const TOKEN = "7274465971:AAHbodjo6mDZTc3zP7MiDUnBaU266TF2gVo";
+
+const TelegramBot = require("node-telegram-bot-api");
+const bot = new TelegramBot(TOKEN, { polling: true });
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+  console.log(msg);
+  if (text === "/start") {
+    bot.sendMessage(
+      chatId,
+      `Assalom alekum ${msg.from.first_name}, 
+Sizga murojat qilishlari uchun 
+iltmos telefon raqamingizni jo'nating.`,
+      {
+        reply_markup: {
+          keyboard: [
+            [
+              {
+                text: "Mahsulotni buyurtma berish",
+                web_app: {
+                  url: "https://express-clone24.netlify.app/",
+                },
+              },
+            ],
+          ],
+        },
+      }
+    );
+  }
+});
